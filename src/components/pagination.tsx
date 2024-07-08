585800227bd42f5caaacb034c547fddb4e5b53be
Esdras Caetano <esdrascaetano7@gmail.com>
2024-07-08 17:40:52 -0300

    dashboard primeiro grafico

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -21,7 +21,7 @@ export function Pagination({
   const pages = Math.ceil(totalCount / perPage) || 1
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-between">
       <span className="text-sm text-muted-foreground">
         Total de {totalCount} item(s)
       </span>
@@ -31,19 +31,19 @@ export function Pagination({
           Página {pageIndex + 1} de {pages}
         </div>
         <div className="flex items-center gap-2">
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-0">
             <ChevronsLeft className="h-4 w-4" />
             <span className="sr-only">Primeira página</span>
           </Button>
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-0">
             <ChevronLeft className="h-4 w-4" />
             <span className="sr-only">Página anterior</span>
           </Button>
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-0">
             <ChevronRight className="h-4 w-4" />
             <span className="sr-only">Próxima página</span>
           </Button>
-          <Button variant="outline" className="h-8 w-8 p-8">
+          <Button variant="outline" className="h-8 w-8 p-0">
             <ChevronsRight className="h-4 w-4" />
             <span className="sr-only">Última página</span>
           </Button>
