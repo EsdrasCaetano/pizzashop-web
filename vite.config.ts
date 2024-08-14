47dcc602f7e3cead3fc7595e6f974aba14997950
unknown <esdrascaetano7@gmail.com>
2024-08-14 17:44:37 -0300

    tratamentos de error order status

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+/// / <reference types="vitest" />
+
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +15,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
